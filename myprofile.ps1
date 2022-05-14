c16253e87f8cf17fc0f258d363ab127297e4ad19
CY-Pan <adredpcy@163.com>
2022-05-14 20:15:13 +0800

    update

--- myprofile.ps1
+++ myprofile.ps1
@@ -8,21 +8,29 @@ function unproxy{
 	ri Env:https_proxy
 }
 
-function showpath{
+function showPath{
 	(gc Env:path) -split ';' | tee -Variable tmp | measure && gv tmp -ValueOnly
 }
 
-function pathsearch($pattern){
+function pathSearch($pattern){
 	(gc Env:path) -split ';' -match $pattern
 }
 
-function pipupdate{
+function pipUpdate{
 	pip list -o --format freeze | %{$_.split('==')[0]} | %{pip install -U $_}
 	pip cache purge
 }
 
 function showSSHlogs($count=10){
 	Get-WinEvent -Path $Env:SystemRoot\System32\Winevt\Logs\OpenSSH%4Operational.evtx -MaxEvents $count
+}
+
+function getHistory($count=50){
+	gc (Get-PSReadlineOption).HistorySavePath -Tail $count
+}
+
+function su{
+	saps wt -Verb RunAs
 }
 
 Set-PSReadLineOption -PredictionSource History
