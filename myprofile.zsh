1b0394fd94110d9788e9246c0c489688a7d6813f
CY-Pan <adredpcy@163.com>
2022-07-07 11:11:08 +0800

    update

---
+++ myprofile.zsh
@@ -0,0 +1,19 @@
+proxy(){
+	export https_proxy=socks5://127.0.0.1:8890 http_proxy=socks5://127.0.0.1:8890 all_proxy=socks5://127.0.0.1:8890
+}
+
+unproxy(){
+	unset https_proxy http_proxy all_proxy
+	export https_proxy http_proxy all_proxy
+}
+
+pipupdate(){
+	pip list -o --format freeze | cut -d = -f 1 | xargs pip install -U
+	pip cache purge
+}
+
+brewupdate(){
+	brew update && brew upgrade && brew autoremove && brew cleanup -s
+}
+
+alias git-shallone='git clone --depth 1'
