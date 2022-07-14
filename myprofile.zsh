09fd48d49caad4da0b730634fb2a67a968d2fcd2
CY-Pan <adredpcy@163.com>
2022-07-15 00:17:00 +0800

    update

--- myprofile.zsh
+++ myprofile.zsh
@@ -1,5 +1,5 @@
 proxy(){
-	export https_proxy=socks5://127.0.0.1:8890 http_proxy=socks5://127.0.0.1:8890 all_proxy=socks5://127.0.0.1:8890
+	export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
 }
 
 unproxy(){
