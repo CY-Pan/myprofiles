3185ecc3742b71963728dad741ab35f39bf26420
CY-Pan <adredpcy@163.com>
2023-04-19 21:46:08 +0800

    update

--- myprofile.ps1
+++ myprofile.ps1
@@ -1,7 +1,7 @@
 function proxy($set=1){
 	switch($set){
-		0 { ri Env:http_proxy
-			ri Env:https_proxy }
+		0 { Remove-Item Env:http_proxy
+			Remove-Item Env:https_proxy }
 		1 { $Env:http_proxy='http://localhost:7890'
 			$Env:https_proxy='http://localhost:7890' }
 		2 { $Env:http_proxy='http://localhost:8889'
@@ -11,17 +11,17 @@ function proxy($set=1){
 
 function showPath($pattern){
 	$tmp = $Env:path -split ';' -match $pattern
-	$tmp | measure -Line
-	gv tmp -ValueOnly
+	$tmp | Measure-Object -Line
+	Get-Variable tmp -ValueOnly
 }
 
 function lt([switch]$Descending){
-	if($Descending.IsPresent){ gci | sort LastWriteTime -Descending }
-	else{ gci | sort LastWriteTime }
+	if($Descending.IsPresent){ Get-ChildItem | Sort-Object LastWriteTime -Descending }
+	else{ Get-ChildItem | Sort-Object LastWriteTime }
 }
 
 function du($dir='.'){
-	gci $dir -Recurse -Force | measure -Property Length -Sum
+	Get-ChildItem $dir -Recurse -Force | Measure-Object -Property Length -Sum
 }
 
 function pipUpdate{
@@ -35,11 +35,11 @@ function showSSHlogs($count=10){
 }
 
 function getHistory($count=50){
-	gc (Get-PSReadlineOption).HistorySavePath -Tail $count
+	Get-Content (Get-PSReadlineOption).HistorySavePath -Tail $count
 }
 
 function su{
-	saps wt -Verb RunAs
+	Start-Process wt -Verb RunAs
 }
 
 function git-shallone([Parameter(Mandatory)]$repo){
@@ -47,24 +47,23 @@ function git-shallone([Parameter(Mandatory)]$repo){
 }
 
 function addRegistryLocations(){
-	if(!(Test-Path HKCR:)){ ndr -PSProvider Registry -Root HKEY_CLASSES_ROOT -Name HKCR -Scope Global }
-	if(!(Test-Path HKCU:)){ ndr -PSProvider Registry -Root HKEY_CURRENT_USER -Name HKCU -Scope Global }
-	if(!(Test-Path HKLM:)){ ndr -PSProvider Registry -Root HKEY_LOCAL_MACHINE -Name HKLM -Scope Global }
-	if(!(Test-Path HKU:)) { ndr -PSProvider Registry -Root HKEY_USERS -Name HKU -Scope Global }
-	if(!(Test-Path HKCC:)){ ndr -PSProvider Registry -Root HKEY_CURRENT_CONFIG -Name HKCC -Scope Global }
+	if(!(Test-Path HKCR:)){ New-PSDrive -PSProvider Registry -Root HKEY_CLASSES_ROOT -Name HKCR -Scope Global }
+	if(!(Test-Path HKCU:)){ New-PSDrive -PSProvider Registry -Root HKEY_CURRENT_USER -Name HKCU -Scope Global }
+	if(!(Test-Path HKLM:)){ New-PSDrive -PSProvider Registry -Root HKEY_LOCAL_MACHINE -Name HKLM -Scope Global }
+	if(!(Test-Path HKU:)) { New-PSDrive -PSProvider Registry -Root HKEY_USERS -Name HKU -Scope Global }
+	if(!(Test-Path HKCC:)){ New-PSDrive -PSProvider Registry -Root HKEY_CURRENT_CONFIG -Name HKCC -Scope Global }
 }
 
 function newRegistryItemForOpen([Parameter(Mandatory)][string]$Ext,
 		[Parameter(Mandatory)][string]$Program,	[Parameter(Mandatory)][string]$IconPath){
-	ni ".$($Ext)_auto_file"
-	ni ".$($Ext)_auto_file\DefaultIcon"
-	ni ".$($Ext)_auto_file\shell"
-	ni ".$($Ext)_auto_file\shell\open"
-	ni ".$($Ext)_auto_file\shell\open\command"
-
-	sp ".$($Ext)_auto_file\DefaultIcon" -Name '(default)' -Value $IconPath
-	sp ".$($Ext)_auto_file\shell\open" -Name '(default)' -Value """$Program"""
-	sp ".$($Ext)_auto_file\shell\open\command" -Name '(default)' -Value """$Program"" ""%1"""
+	New-Item ".$($Ext)_auto_file" && \
+	New-Item ".$($Ext)_auto_file\DefaultIcon" && \
+	New-Item ".$($Ext)_auto_file\shell" && \
+	New-Item ".$($Ext)_auto_file\shell\open" && \
+	New-Item ".$($Ext)_auto_file\shell\open\command" && \
+	Set-ItemProperty ".$($Ext)_auto_file\DefaultIcon" -Name '(default)' -Value $IconPath && \
+	Set-ItemProperty ".$($Ext)_auto_file\shell\open" -Name '(default)' -Value """$Program""" && \
+	Set-ItemProperty ".$($Ext)_auto_file\shell\open\command" -Name '(default)' -Value """$Program"" ""%1""" && \
 }
 
 function dYTaudio([Parameter(Mandatory)]$URL){
@@ -80,20 +79,20 @@ function ft([Parameter(Mandatory,ValueFromPipeline)]$table){
 }
 
 function find([Parameter(Mandatory)]$path, [Parameter(Mandatory)]$name){
-	gci $path -Recurse -Force -Include $name
+	Get-ChildItem $path -Recurse -Force -Include $name
 }
 
 function ln([Parameter(Mandatory)]$src, [Parameter(Mandatory)]$linkpath){
-	ni -ItemType SymbolicLink -Target $src $linkpath
+	New-Item -ItemType SymbolicLink -Target $src $linkpath
 }
 
 function recycle([Parameter(Mandatory, ValueFromRemainingArguments)][string[]]$items){
 	$items | %{
 		if(Test-Path $_ -PathType Leaf){
-			[Microsoft.VisualBasic.FileIO.FileSystem]::DeleteFile($(rvpa $_), 'OnlyErrorDialogs', 'SendToRecycleBin')
+			[Microsoft.VisualBasic.FileIO.FileSystem]::DeleteFile($(Resolve-Path $_), 'OnlyErrorDialogs', 'SendToRecycleBin')
 		}
 		elseif(Test-Path $_ -PathType Container){
-			[Microsoft.VisualBasic.FileIO.FileSystem]::DeleteDirectory($(rvpa $_), 'OnlyErrorDialogs', 'SendToRecycleBin')
+			[Microsoft.VisualBasic.FileIO.FileSystem]::DeleteDirectory($(Resolve-Path $_), 'OnlyErrorDialogs', 'SendToRecycleBin')
 		}
 		else{
 			Write-Error "Path not found: $_"
@@ -126,14 +125,14 @@ Remove-Alias diff -Force
 Remove-Alias rm -Force
 Remove-Alias del -Force
 
-sal open -Value explorer
-sal ri -Value rm -Force
-sal rmdir -Value rm
-sal del -Value rm
+Set-Alias open -Value explorer
+Set-Alias ri -Value rm -Force
+Set-Alias rmdir -Value rm
+Set-Alias del -Value rm
 
 if(!(Test-Path Env:SSH_CONNECTION) -or (Test-Path Env:SSH_TTY)){
 	Set-PSReadLineOption -PredictionSource History
 	Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
 
-	if(Test-Path Env:SSH_CONNECTION){ write "Hello, remote client from $($Env:SSH_CLIENT)." }
+	if(Test-Path Env:SSH_CONNECTION){ Write-Host "Hello, remote client from $($Env:SSH_CLIENT)." }
 }
