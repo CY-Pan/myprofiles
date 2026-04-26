577c64cfecd41879b4d273dd909561e091a7b676
CY-Pan <adredpcy@163.com>
2026-04-26 21:14:05 +0800

    fix: 优化dytaudio

--- myprofile.ps1
+++ myprofile.ps1
@@ -120,8 +120,8 @@ function newRegistryItemForOpen([Parameter(Mandatory)][string]$Ext,
 
 # download youtube audio
 function dYTaudio([Parameter(Mandatory)]$URL, [Parameter(Mandatory)]$cookiefile) {
-	if ($cookiefile -eq '-') { yt-dlp -f ba -x --audio-format mp3 $URL }
-	else { yt-dlp --cookies $cookiefile -f ba -x --audio-format mp3 $URL }
+	if ($cookiefile -eq '-') { yt-dlp -f ba -x $URL }
+	else { yt-dlp --cookies $cookiefile -f ba -x $URL }
 }
 
 # download youtube video
